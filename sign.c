d2e370f684cf41f97c4f4072f9de49050f75c2ed
KUMARESAN <mkumaran1805@gmail.com>
2016-10-20 12:53:09 +0530

    Add files via upload

---
+++ sign.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the numer");
+scanf("%d",a);
+if(a>0)
+
+printf("the number is positive");
+
+else 
+
+printf("the number is negative");
+
+
+return 0;
+}
